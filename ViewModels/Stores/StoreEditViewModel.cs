5f36ee87aa70487c989dfcfee2c2e8e2bf99140b
ElsayedReda98 <elsayedreda760@gmail.com>
2022-04-10 19:19:15 +0200

    Add Validation fot StoreEditViewModel

--- ViewModels/Stores/StoreEditViewModel.cs
+++ ViewModels/Stores/StoreEditViewModel.cs
@@ -1,13 +1,31 @@
-﻿namespace StoreProject.ViewModels.Stores
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreProject.ViewModels.Stores
 {
     public class StoreEditViewModel
     {
+        [Required]
+        [Display(Name ="Store Name")]
+        [StringLength(255)]
         public string StoreName { get; set; }
+        
+        [StringLength(25)]
         public string Phone { get; set; }
+        
+        [StringLength(255)]
         public string Email { get; set; }
+        
+        [StringLength(255)]
         public string Street { get; set; }
+        
+        [StringLength(255)]
         public string City { get; set; }
+
+        [StringLength(10)]
         public string State { get; set; }
+        
+        [Display(Name ="Zip Code")]
+        [StringLength (5),Required]
         public string ZipCode { get; set; }
 
     }
