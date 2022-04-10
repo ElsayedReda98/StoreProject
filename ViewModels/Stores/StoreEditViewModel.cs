f3012988469d711288e987a9b301595f7ee06f57
ElsayedReda98 <elsayedreda760@gmail.com>
2022-04-10 19:15:28 +0200

    Add StoreEditViewModel

---
+++ ViewModels/Stores/StoreEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace StoreProject.ViewModels.Stores
+{
+    public class StoreEditViewModel
+    {
+        public string StoreName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+    }
+}
